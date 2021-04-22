5158e0d26fcd6cfcabbb76846bf232800caf9278
Fanchi852 <fjmo852@gmail.com>
2021-04-22 19:11:10 +0200

    se a creado una primera iteraccion de la UI y se ha añadido al proyecto ademas se ha creado el archivo main con nombre APP y se han añadido algunos cambios en el pom

---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    private Stage stagePrincipal;
+    private VBox vbox;
+
+    public void init() throws Exception {
+        super.init();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
+
+}
