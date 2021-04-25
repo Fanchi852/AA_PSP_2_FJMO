04661f6b200fd4517483b99c8ca6b579e9816cbe
Fanchi852 <fjmo852@gmail.com>
2021-04-25 15:03:31 +0200

    se a pulido el programa terminando la carga de los datos

--- src/main/java/services/ApiService.java
+++ src/main/java/services/ApiService.java
@@ -8,9 +8,25 @@ import rx.Observable;
 
 public interface ApiService {
 
-    @GET("planets/")
-    Observable<Planets> getAllplanetas();
+    @GET("planets/?page=1")
+    Observable<Planets> getPage1planets();
+    @GET("planets/?page=2")
+    Observable<Planets> getPage2planets();
+    @GET("planets/?page=3")
+    Observable<Planets> getPage3planets();
+    @GET("planets/?page=4")
+    Observable<Planets> getPage4planets();
+    @GET("planets/?page=5")
+    Observable<Planets> getPage5planets();
+    @GET("planets/?page=6")
+    Observable<Planets> getPage6planets();
 
-    @GET("starships/")
-    Observable<Starships> getAllstarships();
+    @GET("starships/?page=1")
+    Observable<Starships> getPage1starships();
+    @GET("starships/?page=2")
+    Observable<Starships> getPage2starships();
+    @GET("starships/?page=3")
+    Observable<Starships> getPage3starships();
+    @GET("starships/?page=4")
+    Observable<Starships> getPage4starships();
 }
